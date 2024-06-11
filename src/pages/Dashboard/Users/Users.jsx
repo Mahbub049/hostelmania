e51fdc82d8886679429fefc939fde020f6440cf4
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-06-11 17:04:52 +0600

    search with username implemented and infinite scroller

--- src/pages/Dashboard/Users/Users.jsx
+++ src/pages/Dashboard/Users/Users.jsx
@@ -4,38 +4,66 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 import { FaUsers } from "react-icons/fa";
 import { Chip } from "@material-tailwind/react";
+import { useState } from "react";
 
 const Users = () => {
+  const [searchText, setSearchText] = useState("");
+  const [search, setSearch] = useState("");
   const axiosSecure = useAxiosSecure();
+  console.log(search)
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/users");
+      const res = await axiosSecure.get(`/users?search=${searchText}`);
       return res.data;
     },
   });
-  const handleMakeAdmin = user =>{
-    axiosSecure.patch(`/users/admin/${user._id}`)
-    .then(res =>{
-        console.log(res.data)
-        if(res.data.modifiedCount > 0){
-            refetch();
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: `${user.name} is an Admin Now!`,
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
-    })
-}
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(searchText);
+    refetch();
+  };
   return (
     <div>
       <SectionTitle
         heading="Manage Users"
         subheading="Manage all the users, see their subscription, make admin and details"
       ></SectionTitle>
+      <div>
+        <form onSubmit={handleSearch}>
+          <div className="flex mx-auto mt-6 p-1 overflow-hidden border rounded-lg  w-[400px]">
+            <div className="join">
+              <input
+                onChange={(e) => setSearchText(e.target.value)}
+                value={searchText}
+                className="input input-bordered join-item"
+                type="text"
+                name="search"
+                placeholder="Enter UserName"
+                aria-label="Enter UserName"
+              />
+              <button className="btn text-white bg-blue-400 join-item">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
       <div>
         <div className="mt-12 mx-12">
           <div className="overflow-x-auto">
@@ -58,27 +86,48 @@ const Users = () => {
                     <td>{user.email}</td>
                     <td>
                       {user.role === "admin" ? (
-                          "Admin"
-                        ) : (
-                            <button
-                            onClick={() => handleMakeAdmin(user)}
+                        "Admin"
+                      ) : (
+                        <button
+                          onClick={() => handleMakeAdmin(user)}
                           className="btn px-6 py-2 bg-[#5271FF]"
-                          >
+                        >
                           <FaUsers
                             className="text-white 
                             text-xl"
-                            ></FaUsers>
+                          ></FaUsers>
                         </button>
                       )}
                     </td>
-                      <td>
-                        {
-                            user.badge === "bronze" && <Chip className="w-[150px] mx-auto" color="blue" value={user.badge} /> || 
-                            user.badge === "silver" && <Chip className="w-[150px] mx-auto" value={user.badge}/> || 
-                            user.badge === "gold" && <Chip className="w-[150px] mx-auto" color="amber" value={user.badge} /> || 
-                            user.badge === "platinum" && <Chip className="w-[150px] mx-auto" color="cyan" value={user.badge} /> 
-                        }
-                      </td>
+                    <td>
+                      {(user.badge === "bronze" && (
+                        <Chip
+                          className="w-[150px] mx-auto"
+                          color="blue"
+                          value={user.badge}
+                        />
+                      )) ||
+                        (user.badge === "silver" && (
+                          <Chip
+                            className="w-[150px] mx-auto"
+                            value={user.badge}
+                          />
+                        )) ||
+                        (user.badge === "gold" && (
+                          <Chip
+                            className="w-[150px] mx-auto"
+                            color="amber"
+                            value={user.badge}
+                          />
+                        )) ||
+                        (user.badge === "platinum" && (
+                          <Chip
+                            className="w-[150px] mx-auto"
+                            color="cyan"
+                            value={user.badge}
+                          />
+                        ))}
+                    </td>
                     <td>
                       {/* <button
                         onClick={() => handleDeleteUser(user)}
--- src/pages/Meals/Meals.jsx
+++ src/pages/Meals/Meals.jsx
@@ -3,53 +3,72 @@ import FoodCard from "../../components/FoodCard/FoodCard";
 import Footer from "../../components/Footer/Footer";
 import { StickyNavbar } from "../../components/Navbar/Navbar";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
-import { useInfiniteQuery } from '@tanstack/react-query';
-import InfiniteScroll from 'react-infinite-scroll-component';
-
-const getArticles = async ({ pageParam = 0 }) => {
-    const res = await fetch(`http://localhost:5000/menu?offset=${ pageParam }`);
-    const data = await res.json();
-
-    return { ...data, prevOffset: pageParam }
-
-
-}
+import { useInfiniteQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState } from "react";
+import { Option, Select } from "@material-tailwind/react";
 
 const Meals = () => {
-  //   const axiosPublic = useAxiosPublic();
-  //   const { data: menu = [], refetch } = useQuery({
-  //     queryKey: ["menu"],
-  //     queryFn: async () => {
-  //       const res = await axiosPublic.get("/menu");
-  //       return res.data;
-  //     },
-  //   });
+  const [filter, setFilter] = useState("");
+    // const axiosPublic = useAxiosPublic();
+    // const { data: menu = [], refetch } = useQuery({
+    //   queryKey: ["menu"],
+    //   queryFn: async () => {
+    //     const res = await axiosPublic.get("/menu");
+    //     return res.data;
+    //   },
+    // });
 
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  const getArticles = async ({ pageParam = 0 }) => {
+    const res = await fetch(
+      `http://localhost:5000/menu?filter=${filter}&offset=${pageParam}`
+    );
+    const data = await res.json();
+
+    return { ...data, prevOffset: pageParam };
+  };
+
+  const { data, fetchNextPage, hasNextPage, refetch } = useInfiniteQuery({
     queryKey: ["articles"],
     queryFn: getArticles,
     getNextPageParam: (lastPage) => {
-        if (lastPage.prevOffset + 3 > lastPage.articlesCount) {
-            return false;
-        }
-        return lastPage.prevOffset + 3;
-    }
+      if (lastPage.prevOffset + 3 > lastPage.articlesCount) {
+        return false;
+      }
+      return lastPage.prevOffset + 3;
+    },
+  });
 
-})
-
-const articles = data?.pages.reduce((acc, page) => {
+  const articles = data?.pages.reduce((acc, page) => {
+    refetch();
     const result = Object.values(page).slice(0, -1);
     console.log(result);
-    return [...acc, ...result]
-}, [])
+    return [...acc, ...result];
+  }, []);
 
-console.log(articles);
+  console.log(articles);
   return (
     <div className="container mx-auto">
       <StickyNavbar></StickyNavbar>
       <div>
         <div className="bg-[linear-gradient(45deg,rgba(19,19,24,0.50),rgba(19,19,24,0.50)),url('https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80')] bg-center bg-cover py-20 text-center my-8 rounded-xl mx-3 lg:mx-0">
           <p className="text-4xl  font-bold text-[#78c9f4] mons">Menu</p>
+          <div>
+            <select
+              onChange={(e) => {
+                setFilter(e.target.value);
+              }}
+              value={filter}
+              name="category"
+              id="category"
+              className="border p-4 rounded-lg"
+            >
+              <option value="">Filter By Category</option>
+              <option value="breakfast">Breakfast</option>
+              <option value="lunch">Lunch</option>
+              <option value="dinner">Dinner</option>
+            </select>
+          </div>
         </div>
         <InfiniteScroll
           dataLength={articles ? articles.length : 0}
@@ -57,13 +76,13 @@ console.log(articles);
           hasMore={hasNextPage}
           loading={<div>Loading...☝️</div>}
         >
-        <div className="mt-4 grid grid-cols-3">
-          {articles && articles.map((item) => {
-            return(
-                <FoodCard key={item.id} item={item}></FoodCard>
-              )
-          })}s
-        </div>
+          <div className="mt-4 grid grid-cols-3">
+            {articles &&
+              articles.map((item) => {
+                return <FoodCard key={item.id} item={item}></FoodCard>;
+              })}
+            s
+          </div>
         </InfiniteScroll>
       </div>
       <Footer></Footer>
